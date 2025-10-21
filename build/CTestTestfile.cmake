e3070bdbcf222f6ddfb447f5a8d21087b3552b00
Your Name <you@example.com>
2025-10-21 11:57:06 +0800

    Stop tracking logs folder

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user_dell02/simzx_ws/ros1_inspire_hand/src
-# Build directory: /home/user_dell02/simzx_ws/ros1_inspire_hand/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("ros1_inspire_hand")
